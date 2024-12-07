16ba194b8f106aa6027c88fe8aac2fe5e390d8bd
Kiernan McGuigan <46582079+kmcguigan1@users.noreply.github.com>
2024-12-07 16:17:36 -0500

    updated website
    updated the website text and reformatted the sizes and the flex

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
 		<div className={styles.app}>
 			<Navbar />
 			<HomePage />
-			<Footer />
 		</div>
 	);
 }
--- src/components/HomePage/HeroText.js
+++ src/components/HomePage/HeroText.js
@@ -1,7 +1,5 @@
 import styles from "./HomePage.module.scss";
 
-import HeroIcons from "./HeroIcons";
-
 const HeroText = () => {
 	return (
 		<div className={styles.textContainer}>
@@ -11,9 +9,8 @@ const HeroText = () => {
 				<span className={styles.highlightText}>Masters</span> at the{" "} 
 				<span className={styles.highlightText}>University of Waterloo</span> in the{" "} 
 				<span className={styles.highlightText}>Vision Image Processing Lab</span>. 
-				My research focuses on long-range high-resolution spatio temporal forecasting with 
-				applications to sea ice concentration in the Canadian Arctic. However, on the side I also
-				enjoy working on sports analytics and with novel view sythesis techniques such as NeRF and Gaussian Splatting.
+				My research focuses on spatio-temporal deep learning methods with applications to 
+				Arctic sea ice forcasting.
 			</p>
 			<p className={styles.text}>
 				I completed my undergraduate degree in <span className={styles.highlightText}>Software Engineering</span> at the{" "}
@@ -21,20 +18,12 @@ const HeroText = () => {
 				write readable, flexible, and deployable machine learning code. 
 			</p>
 			<p className={styles.text}>
-				While I predominantly write my code in{" "} <span className={styles.highlightText}>Python</span>
+				While I predominantly code in{" "} <span className={styles.highlightText}>Python</span>{" "} 
 				using tools like{" "} <span className={styles.highlightText}>PyTorch</span> and{" "} 
 				<span className={styles.highlightText}>Tensorflow</span>, I still try to spend time to keep up 
 				my{" "} <span className={styles.highlightText}>Java</span>,{" "} <span className={styles.highlightText}>C/C++</span>,
 				and{" "} <span className={styles.highlightText}>Web Development</span> skills as well.
 			</p>
-			<p className={styles.text}>
-				If you're interested in any of my work check me out on{" "}
-				<span className={styles.highlightText}>GitHub</span> or{" "}
-				<span className={styles.highlightText}>Kaggle</span>. Alternative feel free to reach out on{" "}
-				<span className={styles.highlightText}>LinkedIn</span>, I'm always happy to meet new people in
-				the tech space!
-			</p>
-			{/* <HeroIcons /> */}
 		</div>
 	);
 };
